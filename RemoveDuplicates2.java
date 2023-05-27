9c1273dd1a95dda2066af0faf19d6ac955289f81
Saurabh Bansal <saurabhbansal2443@gmail.com>
2023-05-27 18:13:11 +0530

    ;

--- RemoveDuplicates2.java
+++ RemoveDuplicates2.java
@@ -1,38 +1,40 @@
 import java.util.*;
+
 public class RemoveDuplicates2 {
     public static void main(String[] args) {
-        int[] arr ={1,0,2,2,1,3,4,5,3,6,6,7};
+        int[] arr = { 1, 0, 2, 2, 1, 3, 4, 5, 3, 6, 6, 7 };
 
-        ArrayList<Integer> ans=new ArrayList<>();
+        ArrayList<Integer> ans = new ArrayList<>();
 
+        for (int i = 0; i < arr.length; i++) {
 
-        for(int i=0;i<arr.length;i++){
+           boolean res = false;
 
-            boolean res=checkElement(ans, arr[i]);
+           for(int j=0;j<ans.size();j++){
+             if(ans.get(j)==arr[i]){
+                res=true;
+             }
+           }
 
-            if(res==false){
+            if (res == false) {
                 ans.add(arr[i]);
             }
         }
 
-        for(int i : ans){
+        for (int i : ans) {
             System.out.println(i);
         }
 
-
-
-
     }
 
+    public static boolean checkElement(ArrayList<Integer> list, int k) {
 
-    public static boolean checkElement(ArrayList<Integer> list, int k ){
+        boolean flag = false;
 
-        boolean flag= false;
+        for (int i = 0; i < list.size(); i++) {
 
-        for(int i=0;i<list.size();i++){
-
-            if(list.get(i)==k){
-                flag=true;
+            if (list.get(i) == k) {
+                flag = true;
             }
         }
 
@@ -40,6 +42,4 @@ public class RemoveDuplicates2 {
 
     }
 
-
-   
 }
